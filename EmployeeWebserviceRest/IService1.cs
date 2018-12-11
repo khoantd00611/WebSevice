ece5ba431ee3394a155173ff8924b11ce4e523c0
khoantd00611 <khoantd00611@fpt.edu.vn>
2018-12-11 09:34:43 +0700

    hoanthanh

---
+++ EmployeeWebserviceRest/IService1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeeWebserviceRest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "GetProductList/")]
+        List<Employee> GetProductList();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "PostEmployee/")]
+        bool AddEmployee(Employee eml);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "PutEmployee/")]
+        bool UpdateEmployee(Employee eml);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE",
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "DeleteEmployee/")]
+        bool DeleteEmployee(int idE);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ EmployeeWebserviceRest/Service1.svc.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeeWebserviceRest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        EmployeeDataDataContext data = new EmployeeDataDataContext();
+        public bool AddEmployee(Employee eml)
+        {
+            try
+            {
+                data.Employee.InsertOnSubmit(eml);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteEmployee(int idE)
+        {
+            try
+            {
+                Employee employeeToDelete = (from employee in data.Employee where employee.empId == idE select employee).Single();
+                data.Employee.DeleteOnSubmit(employeeToDelete);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public List<Employee> GetProductList()
+        {
+            try
+            {
+                return (from employee in data.Employee select employee).ToList();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool UpdateEmployee(Employee eml)
+        {
+            Employee employeeToModify = (from employee in data.Employee where employee.empId == eml.empId select employee).Single();
+            employeeToModify.age = eml.age;
+            employeeToModify.address = eml.address;
+            employeeToModify.firstName = eml.firstName;
+            employeeToModify.lastName = eml.lastName;
+            data.SubmitChanges();
+            return true;
+        }
+    }
+}
